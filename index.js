580e7ed1ff4116aaa4cd4052046d99723aedcdc9
taffy <taffik@yandex.ru>
2019-02-19 15:47:12 +0300

    add parser urls

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 require('dotenv').config();
 const Telegraf = require('telegraf');
 const getData = require('./getData');
+const parseURL = require('./parseURL');
 const validator = require('validator');
-
 const bot = new Telegraf(process.env.TOKEN);
 bot.start((ctx) => {
     ctx.reply('👋 Привет!\n\nПоделись со мной ссылкой на трек или альбом из любого приложения, а я в ответ пришлю ссылки, на все музыкальные сервисы где можно найти этот альбом или композицию.')
 });
-
 bot.help((ctx) => ctx.reply('Никогда ни на кого не обижайся. Ты человека прости или убей. © Иосиф Сталин'));
 
 bot.hears('тотален', (ctx) => ctx.reply('100% пидор'));
 
 bot.on('message', async (ctx) => {
     const message = ctx.message.text;
-
-    if (message) {
+    const urls = parseURL(message);
+    if (urls[0]) {
         try {
-            if (validator.isURL(message)) {
+            if (validator.isURL(urls[0])) {
                 const sendLinks = async () => {
                     ctx.reply('🚬 Подождите немного, пока я ищу ссылки...');
-                    const data = await getData({ link: message });
+                    const data = await getData({ link: urls[0] });
                     if (data) {
                         let links = '';
                         data.songlink.links.listen.forEach(item => {
---
+++ parseURL.js
@@ -0,0 +1,4 @@
+module.exports = function(text) {
+    const regexp = /(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/g
+    return text.match(regexp) || []
+}
